64b8230d4d2668f051df909db60e42868fd0a38e
Chris Witko <chris.witko@gmail.com>
2018-06-01 08:36:51 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // first we will make a new context
 const MyContext = React.createContext();
@@ -6,48 +6,50 @@ const MyContext = React.createContext();
 // Then create a provider Component
 class MyProvider extends Component {
   state = {
-    name: 'Wes',
+    name: "Wes",
     age: 100,
     cool: true
-  }
+  };
   render() {
     return (
-      <MyContext.Provider value={{
-        state: this.state,
-        growAYearOlder: () => this.setState({
-          age: this.state.age + 1
-        })
-      }}>
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () =>
+            this.setState({
+              age: this.state.age + 1
+            })
+        }}
+      >
         {this.props.children}
       </MyContext.Provider>
-    )
+    );
   }
 }
 
-const Family = (props) => (
+const Family = props => (
   <div className="family">
     <Person />
   </div>
-)
+);
 
 class Person extends Component {
   render() {
     return (
       <div className="person">
         <MyContext.Consumer>
-          {(context) => (
+          {context => (
             <React.Fragment>
               <p>Age: {context.state.age}</p>
-              <p>Name: {context.state.name}</p>
+              <p>First name: {context.state.name}</p>
               <button onClick={context.growAYearOlder}>🍰🍥🎂</button>
             </React.Fragment>
           )}
         </MyContext.Consumer>
       </div>
-    )
+    );
   }
 }
-
 
 class App extends Component {
   render() {
@@ -61,6 +63,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
